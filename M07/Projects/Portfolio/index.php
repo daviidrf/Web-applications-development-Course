7090dc5720db2a2b2cae5dc96aa1e0bf87d329ee
David Rodriguez <davidrodriguez1470@gmail.com>
2021-10-05 14:43:29 +0200

    Change Practice Icons

--- M07/Projects/Portfolio/index.php
+++ M07/Projects/Portfolio/index.php
@@ -88,7 +88,7 @@ header_nav("Inicio");
 
                 <div class="col-md-3 col-sm-6">
                     <a href="ex2.php" class="rotate-box-1 square-icon wow zoomIn" data-wow-delay="0.2s">
-                        <span class="rotate-box-icon"><i class="fa fa-diamond"></i></span>
+                        <span class="rotate-box-icon"><i class="fab fa-galactic-republic"></i></span>
                         <div class="rotate-box-info">
                             <h4>PRACTICA 2</h4>
                             <p>DNI, el usuario introduce un DNI y se verifica si es válido o no.</p>
@@ -98,7 +98,7 @@ header_nav("Inicio");
 
                 <div class="col-md-3 col-sm-6">
                     <a href="ex3.php" class="rotate-box-1 square-icon wow zoomIn" data-wow-delay="0.4s">
-                        <span class="rotate-box-icon"><i class="fa fa-heart"></i></span>
+                        <span class="rotate-box-icon"><i class="fab fa-galactic-republic"></i></span>
                         <div class="rotate-box-info">
                             <h4>PRACTICA 3</h4>
                             <p>Palindromos, el usuario introduce una frase o palabra y se verifica si es palindroma o no.</p>
@@ -108,7 +108,7 @@ header_nav("Inicio");
 
                 <div class="col-md-3 col-sm-6">
                     <a href="#" class="rotate-box-1 square-icon wow zoomIn" data-wow-delay="0.6s">
-                        <span class="rotate-box-icon"><i class="fa fa-clock-o"></i></span>
+                        <span class="rotate-box-icon"><i class="fab fa-galactic-republic"></i></span>
                         <div class="rotate-box-info">
                             <h4>PRACTICA 4</h4>
                             <p>Lorem ipsum dolor sit amet set, consectetur utes anet adipisicing elit, sed do
