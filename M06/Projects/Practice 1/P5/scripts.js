5d08f703568370d658c97603f1d1ac7faaa28926
Davex <davidig1470@gmail.com>
2021-10-11 17:06:30 +0200

    Updated M06 - Excercise 5

--- M06/Projects/Practice 1/P5/scripts.js
+++ M06/Projects/Practice 1/P5/scripts.js
@@ -3,70 +3,43 @@ document.addEventListener("DOMContentLoaded", function () {
 }, false);
 
 function procesar() {
+    //OCULTAR CONTENEDOR DE SALIDA DE DATOS
+    document.getElementById("showResult").style.display = "none";
+    document.getElementById("showResult").style.border = "0px solid black";
+
+    //BORRAR MEMORIA DE ERRORES PARA QUE NO SE REPITAN LOS ERRORES
+    deleteMemory(document.getElementById("nameMemory"),
+        document.getElementById("surnameMemory"),
+        document.getElementById("ageMemory"));
+
     let name = document.getElementById("name").value;
     let surname = document.getElementById("surname").value;
     let age = parseInt(document.getElementById("age").value);
 
-    calcularErrores(name, surname, age);
-
-    if(calcularErrores(name, surname, age) == true){
+    //CALCULA LOS ERRORES Y VERIFICA SI MOSTRAR RESULTADO O NO
+    if (calcularErrores(name, surname, age)) {
         returnHtml(name, surname, age);
-    }    
-
-}
-
-function calcularErrores (name, surname, age){
-    let check = true;
-    
-    //IF'S DE NAME
-    if (validarNameNull(name) == false) {
-        console.log("El nombre es nulo!");
-        // let error = document.createElement("p");
-        // error.innerText = "El nombre introducido es nulo!";
-        // document.getElementById("resultado").appendChild(error);
-        check = false;
     }
-    if(validarNameLength(name) == false){
-        console.log("La longitud del nombre no puede ser 0!");
-        check = false;
-    }
-    
-    //IF'S DE SURNAME
-    if(validarNameNull(surname) == false){
-        console.log("En apellido es nulo!");
-        check = false;
-    }
-    if(validarNameLength(surname) == false){
-        console.log("La longitud del apellido no puede ser 0!");
-        check = false;
-    }
-
-    //IF'S DE AGE
-    if(validarAgeLenght(age) == false){
-        console.log("La longitud del número no puedes ser 0");
-        check = false;
-    }
-    if(validarAgeNull(age) == false){
-        console.log("El número es nulo");
-        check = false;
-    }
-    if(validarAgeNaN(age)){
-        console.log("No es un numero");
-        check = false;
-    }
-    
-    return check;
 }
 
 function returnHtml(name, surname, age) {
 
+    deleteMemory(document.getElementById("showNameMemory"),
+        document.getElementById("showSurnameMemory"),
+        document.getElementById("showAgeMemory"));
+
+    //CREAR SALIDAS DE DATOS
     let nameP = document.createElement("p");
     nameP.innerText = "Nombre --> " + name;
+    nameP.setAttribute('id', 'showNameMemory');
     let surnameP = document.createElement("p");
     surnameP.innerText = "Apellido --> " + surname;
+    surnameP.setAttribute('id', 'showSurnameMemory');
     let ageP = document.createElement("p");
     ageP.innerText = "Edad --> " + age;
+    ageP.setAttribute('id', 'showAgeMemory');
 
+    //AÑADIR SALIDAS DE DATOS
     document.getElementById("resultado").appendChild(nameP);
     document.getElementById("resultado").appendChild(surnameP);
     document.getElementById("resultado").appendChild(ageP);
@@ -75,37 +48,139 @@ function returnHtml(name, surname, age) {
     document.getElementById("showResult").style.border = "2px solid black";
 }
 
+////////////////////////////////////////BORRAR MEMORIA////////////////////////////////////////////
+
+function deleteMemory(nameMemory, surnameMemory, ageMemory) {
+
+    if (nameMemory != null) {
+        nameMemory.remove();
+    }
+    if (surnameMemory != null) {
+        surnameMemory.remove();
+    }
+    if (ageMemory != null) {
+        ageMemory.remove();
+    }
+}
+
+/////////////////////////////////////////VALIDACIONES/////////////////////////////////////////////
+
+function calcularErrores(name, surname, age) {
+    let check = true;
+
+    //IF'S DE NAME
+    if (!validarStringNull(name) || !validarStringLength(name)) {
+        console.log("El nombre no puede estar vacio!");
+        check = false;
+
+        let nameError = document.createElement("p");
+        nameError.innerText = "El nombre no puede estar vacio!";
+        nameError.setAttribute('id', 'nameMemory');
+        document.getElementById("nameError").appendChild(nameError);
+
+        document.getElementById("nameError").style.display = "block";
+        document.getElementById("nameError").style.color = "red";
+        document.getElementById("nameError").style.fontWeight = "bold";
+    }
+
+    //IF'S DE SURNAME
+    if (!validarStringNull(surname) || !validarStringLength(surname)) {
+        console.log("El apellido no puede estar vacio!");
+        check = false;
+
+        let surnameError = document.createElement("p");
+        surnameError.innerText = "El apellido no puede estar vacio!";
+        surnameError.setAttribute('id', 'surnameMemory');
+        document.getElementById("surnameError").appendChild(surnameError);
+
+        document.getElementById("surnameError").style.display = "block";
+        document.getElementById("surnameError").style.color = "red";
+        document.getElementById("surnameError").style.fontWeight = "bold";
+    }
+
+    //IF'S DE AGE
+    if (!validarAgeLenght(age) || !validarAgeNull(age)) {
+        console.log("La edad no puede estar vacio!");
+        check = false;
+
+        let ageError = document.createElement("p");
+        ageError.innerText = "La edad no puede estar vacio!";
+        ageError.setAttribute('id', 'ageMemory');
+        document.getElementById("ageError").appendChild(ageError);
+
+        document.getElementById("ageError").style.display = "block";
+        document.getElementById("ageError").style.color = "red";
+        document.getElementById("ageError").style.fontWeight = "bold";
+    }
+
+    if (validarAgeNaN(age)) {
+        console.log("No es un numero!");
+        check = false;
+
+        let ageError = document.createElement("p");
+        ageError.innerText = "No es un numero!";
+        ageError.setAttribute('id', 'ageMemory');
+        document.getElementById("ageError").appendChild(ageError);
+
+        document.getElementById("ageError").style.display = "block";
+        document.getElementById("ageError").style.color = "red";
+        document.getElementById("ageError").style.fontWeight = "bold";
+    }
+
+    if (!validarAgeRango) {
+        console.log("La edad tiene que ser mayor a 0!");
+        check = false;
+
+        let ageError = document.createElement("p");
+        ageError.innerText = "La edad tiene que ser mayor a 0!";
+        ageError.setAttribute('id', 'ageMemory');
+        document.getElementById("ageError").appendChild(ageError);
+
+        document.getElementById("ageError").style.display = "block";
+        document.getElementById("ageError").style.color = "red";
+        document.getElementById("ageError").style.fontWeight = "bold";
+    }
+
+
+    return check;
+}
+
 //FUNCIONES NAME
-function validarNameNull(name) {
+function validarStringNull(name) {
     if (name == null) {
         return false;
     }
     return true;
 }
-
-function validarNameLength(name) {
+function validarStringLength(name) {
     if (name.length == 0) {
         return false;
     }
     return true;
 }
 
-
 //FUNCIONES AGE
 function validarAgeNull(age) {
     if (age == null) {
         return false;
+    } else {
+        return true;
     }
-    return true;
 }
-
 function validarAgeLenght(age) {
-    let num = age;
-    if (`${num}`.length == 0) {
+    if (`${age}`.length <= 0) {
         return false;
+    } else {
+        return true;
     }
-    return true;
 }
 function validarAgeNaN(age) {
     return isNaN(age);
+}
+function validarAgeRango(age) {
+    if (age <= 0) {
+        return false;
+    } else {
+        return true;
+    }
 }
