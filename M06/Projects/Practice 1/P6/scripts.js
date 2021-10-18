c4a1b856781dcb2aa55f7b2881dab926dc51f29d
daviidrf <davidig1470@gmail.com>
2021-10-18 14:54:57 +0200

    Update M06 Memorama

--- M06/Projects/Practice 1/P6/scripts.js
+++ M06/Projects/Practice 1/P6/scripts.js
@@ -4,23 +4,20 @@ var colors = ["green", "green", "red", "red", "yellow",
     "purple"];
 colors = order(colors);
 
-var jugada1;
-var jugada1ID;
-var aciertos = 0;
+var seleccion1; //First card selection.
+var seleccion2; //Second card selection.
+var contador = 0; //Counter to know when show or hide the cards.
+var aciertos = 0; //Conter to know how many successful pairs are made.
 
 document.addEventListener("DOMContentLoaded", function () {
 
     document.addEventListener("submit", generar);
     let juego = document.getElementById("resultado");
-
-    //juego.addEventListener("click", showFirstCase);
-    juego.addEventListener("click", provepair);
-
-
-
+    juego.addEventListener("click", makePair);
 
 }, false);
 
+//This function validate if the user introduced is in the correct format.
 function generar() {
     document.getElementById("showResult").style.display = "none";
     document.getElementById("showResult").style.border = "0px solid black";
@@ -37,13 +34,16 @@ function generar() {
     }
 }
 
+//This function generate the Memoram in the HTML file.
 function generarMemorama() {
+    let resultado = document.getElementById("resultado");
+
     let colorNum = 0;
     for (let row = 0; row < 4; row++) {
         let fila = document.createElement("div");
         fila.setAttribute('id', 'row' + row);
         fila.setAttribute('class', 'row');
-        document.getElementById("resultado").appendChild(fila);
+        resultado.appendChild(fila);
         for (let col = 0; col < 4; col++) {
             let casilla = document.createElement("div");
             casilla.setAttribute('id', colorNum);
@@ -54,76 +54,55 @@ function generarMemorama() {
         }
 
     }
+    let marcador = document.createElement("div");
+    marcador.innerHTML = "<p>Aciertos: " + aciertos + "/8</p>"
+    resultado.insertBefore(marcador, resultado);
 }
 
-function showFirstCase(e) {
-    jugada1 = e.target.classList;
-    jugada1ID = e.target.id;
+//This function validate the card pairs and hide or show them.
+function makePair(e) {
 
-    e.target.classList.remove("default");
+    if (!e.target.classList.contains("girado") && aciertos < 8) {
+        if (contador == 0) {
+            seleccion1 = e.target.classList;
+            seleccion1.remove("default");
+            seleccion1.add("girado");
+            contador++;
+        }
+        else if (contador == 1) {
+            seleccion2 = e.target.classList;
+            seleccion2.remove("default");
+            seleccion2.add("girado");
+            contador++;
+        }
+        if (contador == 2) {
+            console.log(seleccion1[0]);
+            console.log(seleccion2[0]);
+            console.log("---------");
+            if (seleccion1[0] != seleccion2[0]) {
+                setTimeout(function () {
+                    seleccion1.add("default");
+                    seleccion2.add("default");
+                    seleccion1.remove("girado");
+                    seleccion2.remove("girado");
+                }, 800);
 
-    console.log(jugada1[0]);
-    console.log(jugada1ID);
+            }
+            else {
+                aciertos++;
+            }
+            contador = 0;
+            console.log(aciertos);
+        }
+    }
 }
 
-function provepair(e) {
-    var jugada2;
-    var jugada2ID;
-
-    jugada1 = e.target.classList;
-    jugada1ID = e.target.id;
-
-    jugada2 = e.target.classList;
-    jugada2ID = e.target.id;
-
-    console.log(jugada1[0]);
-    console.log(jugada1ID);
-
-    console.log(jugada2[0]);
-    console.log(jugada2ID);
-
-
-
-
-
-
-
-    // for (let i = 0; i < colors.length; i++) {
-    //     //console.log(colors[i]);
-    //     //console.log(e.target);
-    // }
-
-    // var arrayClases = [16];
-    // for (let i = 0; i < 8; i++) {
-
-    //     for (let j = 0; j < 2; j++) {
-    //         casilla1 = e.currentTarget.classList;
-    //         console.log(casilla1[0]);
-    //         casilla2 = e.currentTarget.classList;
-    //         console.log(casilla2[0]);
-    //     }
-    //arrayClases[i] = e.target.classList;
-
-    //console.log(arrayClases[i][0]);
-    //}
-    // for (let i = 0; i < arrayClases.length; i++) {
-    //     const element = array[index];
-
-    // }
-
-    //console.log(arrayClases.classList);
-
-
-
-    //e.target.classList.remove("default");
-}
-
+//This function order randomly the array of colors.
 function order(colors) {
     return colors.sort(function () {
         return Math.random() - 0.5;
     });
 }
-
 
 //VALIDACIONES Y ERRORES
 
@@ -132,7 +111,6 @@ function deleteMemory(userMemory) {
         userMemory.remove();
     }
 }
-
 function calcularErrores(user) {
     let check = true;
 
@@ -164,7 +142,6 @@ function calcularErrores(user) {
     }
     return check;
 }
-
 function isUserNameValid(user) {
 
     let required = /^[A-Z][a-z0-9]{4,8}$/;
