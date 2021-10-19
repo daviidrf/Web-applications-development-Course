e348a00a7fa061baf551bc65a2d6fff5457d260f
daviidrf <davidig1470@gmail.com>
2021-10-19 13:12:30 +0200

    M07 Add Formulario

---
+++ M07/Projects/Formulario/checkAnswers.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'functions.php';
+header_nav("PT AVALUABLE 1");
+?>
+<div class="mx-auto" style="text-align: center;margin-top:8rem;" id="formulario">
+
+<h1>REVISANDO RESPUESTAS</h1>
+
+</div>
+<?php
+jvCharge();
+?>
+</body>
+
+</html>
